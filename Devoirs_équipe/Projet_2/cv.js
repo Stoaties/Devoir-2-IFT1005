1551b7033bef84fdcc31f43404d55ee8ad1dc3fb
CLariviere <70656083+CLariviere@users.noreply.github.com>
2020-11-30 15:36:21 -0500

    submit form alert

--- Devoirs_équipe/Projet_2/cv.js
+++ Devoirs_équipe/Projet_2/cv.js
@@ -15,31 +15,34 @@ $.getJSON('cv.json', function (data) {
 		expList.push(expElem);
 	}
 	var temp2 = expList.toString();
-	var temp3 = temp2.replace(/,/g,"<br>");
-	document.getElementById("experiences").innerHTML=temp3;
+	var temp3 = temp2.replace(/,/g, "<br>");
+	document.getElementById("experiences").innerHTML = temp3;
 
-			var hobbies = data.hobby.toString();
-			var temp = hobbies.replace(/,/g,"<br>");
-			document.getElementById("passe-temps").innerHTML = temp;
+	var hobbies = data.hobby.toString();
+	var temp = hobbies.replace(/,/g, "<br>");
+	document.getElementById("passe-temps").innerHTML = temp;
 
-			var formation = data.formation;
-			var formString = "";
-			for (var i = 0; i < formation.length; i++) {
-				var temp = formation[i].toString();				
-				if(i==formation.length-1){
-					formString = formString + temp;
-				}else{
-					formString = formString + temp +";";
-				}
-			}
-			var formations =  formString.replace(/;/g,"<br>");
-			document.getElementById("formation").innerHTML = formations;
-			console.log(formString);
-			
-			var images = "";
-			for (var i = 0; i < data.photo.length; i++) {
-				images = images + "<img class = 'varia-img-container' src='images/" + data.photo[i] + "'>";
-				console.log(images);
-			}
-			document.getElementById("varia-images").innerHTML = images;
-		});
+	var formation = data.formation;
+	var formString = "";
+	for (var i = 0; i < formation.length; i++) {
+		var temp = formation[i].toString();
+		if (i == formation.length - 1) {
+			formString = formString + temp;
+		} else {
+			formString = formString + temp + ";";
+		}
+	}
+	var formations = formString.replace(/;/g, "<br>");
+	document.getElementById("formation").innerHTML = formations;
+	console.log(formString);
+
+	var images = "";
+	for (var i = 0; i < data.photo.length; i++) {
+		images = images + "<img class = 'varia-img-container' src='images/" + data.photo[i] + "'>";
+		console.log(images);
+	}
+	document.getElementById("varia-images").innerHTML = images;
+});
+function help() {
+	alert("Vous serez sauvé dès que possible!");
+}
